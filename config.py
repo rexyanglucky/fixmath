9efd07200cc3b764029eddc146cc3024887f0f03
lfofiug <lfofaty@msn.com>
2017-11-21 21:49:22 +0800

    修改样式

--- config.py
+++ config.py
@@ -1,29 +1,27 @@
 #! /usr/bin/python
 # encoding: utf-8
-
 '''
 系统的全局配置参数与URL
 '''
 
 import web
 
-
 FIX_MATH_CONFIG = web.storage(
-    name="fixMath",
-    url="https://www.fixmath.com",
-    keywords="ajorm.com,zh-cn.vip,fixmath.com,luyuhai.com",
-    description="fixMath---将部分复杂的数学原理转化为计算机方面的应用",
-    port=int(web.os.getenv('PORT') or 8080),
-    author="Mr.Lu",
-    template_dir="template",
-    home_url="/",
-    static_url="/static",
-    url_suffix=".html",
-    favicon_url="/favicon.ico",
-    init_day="2017/11/21",
-    init_time="19:27:25",
-    cache=False,
-    debug=True
+    name="fixMath",                                                 #fixmath名称
+    url="https://www.fixmath.com",                                  #fixmath域名
+    keywords="ajorm.com,zh-cn.vip,fixmath.com,luyuhai.com",         #fixmath关键字
+    description="fixMath---将部分复杂的数学原理转化为计算机方面的应用",    #fixmath介绍
+    port=int(web.os.getenv('PORT') or 8080),                        #默认端口
+    author="Mr.Lu",                                                 #作者
+    template_dir="template",                                        #模板位置
+    home_url="/",                                                   #首页
+    static_url="/static",                                           #静态文件位置
+    calculus_url="/zh-cn",                                          #微积分位置
+    favicon_url="/favicon.ico",                                     #fixmath图标位置
+    init_day="2017/11/21",                                          #fixmath初始化day
+    init_time="19:27:25",                                           #fixmath初始化time
+    cache=False,                                                    #fixmath缓存
+    debug=True                                                      #fixmath调试
 )
 
 web.config.port = FIX_MATH_CONFIG.port
